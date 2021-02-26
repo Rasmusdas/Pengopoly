f81292ee5ea2859d7211702d2c53cc32d4cb97b1
Rasmusdas <rasmusmkn@gmail.com>
2021-02-26 18:10:53 +0100

    Thing can move across board pog

--- Assets/Scripts/HumanPlayer.cs
+++ Assets/Scripts/HumanPlayer.cs
@@ -15,7 +15,7 @@ public class HumanPlayer : Player
 
     public override void StartTurn(Action endTurn)
     {
-        StartCoroutine(MoveToSpace((position + 1) % board.Size));
+        StartCoroutine(MoveToSpace(board.Size));
     }
 
     public void ShowInterface()
@@ -25,16 +25,20 @@ public class HumanPlayer : Player
 
     public IEnumerator MoveToSpace(int index)
     {
-        for (int i = position; i < index; i++)
+
+        for (int i = position+1; i < index; i++)
         {
+            Vector3 startPosition = transform.position;
             float t = -2;
-            while(t <= 2)
+            transform.LookAt(board[i].position + Vector3.up);
+            while (t <= 2.001)
             {
-                transform.position = new Vector3(Mathf.Lerp(transform.position.x,board[i].position.x,(t+2)/4),  5-(t*t), Mathf.Lerp(transform.position.z, board[i].position.z, (t+2)/4));
-                yield return new WaitForSeconds(0.01f);
-                Debug.Log((t + 2) / 4.0f);
-                t += 0.05f;
+                transform.position = new Vector3(Mathf.Lerp(startPosition.x, board[i].position.x, (t + 2) / 4.0f), 3 - (t * t)*1/2, Mathf.Lerp(startPosition.z, board[i].position.z, (t + 2) / 4.0f));
+                
+                yield return new WaitForSeconds(0.009f);
+                t += 2f/10f;
             }
         }
+
     }
 }
