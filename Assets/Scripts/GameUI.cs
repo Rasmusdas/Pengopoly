83b4896df0534176e0215b6c784cc3ef47b9cd82
Rasmusdas <rasmusmkn@gmail.com>
2021-02-26 20:18:01 +0100

    Added missing files

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    public Text[] fishMeters;
+    public Board board;
+    private void Start()
+    {
+        for (int i = 0; i < board.players.Length; i++)
+        {
+            fishMeters[i].gameObject.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < board.players.Length; i++)
+        {
+            fishMeters[i].text = board.players[i].fish + "";
+        }
+    }
+}
---
+++ Assets/Scripts/PropertyUI.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PropertyUI : MonoBehaviour
+{
+    public Button buyButton;
+
+    public GameObject notOwnedButtons;
+
+    public GameObject ownedButtons;
+
+    public PropertySpace prop;
+
+    Player currentPlayer;
+    public void ShowInterface(Player currentPlayer)
+    {
+        gameObject.SetActive(true);
+        this.currentPlayer = currentPlayer;
+
+        if(prop.owner)
+        {
+            notOwnedButtons.SetActive(false);
+            ownedButtons.SetActive(true);
+        }
+        else
+        {
+            notOwnedButtons.SetActive(true);
+            ownedButtons.SetActive(false);
+        }
+
+        if(currentPlayer.fish < prop.price )
+        {
+            buyButton.interactable = false;
+        }
+    }
+
+    public void BuyProperty()
+    {
+        currentPlayer.fish -= prop.price;
+        prop.owner = currentPlayer;
+        HideInterface();
+    }
+
+    public void HideInterface()
+    {
+        gameObject.SetActive(false);
+    }
+}
